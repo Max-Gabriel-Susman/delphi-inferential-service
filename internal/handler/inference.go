3e2330b58194b4ef0566b039476e1888b72c5b43
Gabe Susman <36492747+Max-Gabriel-Susman@users.noreply.github.com>
2023-08-19 09:02:42 -0600

    partially implemented token gen endpoint

--- internal/handler/inference.go
+++ internal/handler/inference.go
@@ -17,6 +17,7 @@ func InferenceEndpoints(app *delphiweb.App, api *inference.API) {
 	ig := inferenceGroup{API: api}
 
 	app.Handle("GET", "/health", ig.HealthCheck)
+	app.Handle("GET", "/generate", ig.GenerateTokens)
 }
 
 func (ig inferenceGroup) HealthCheck(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
@@ -25,6 +26,16 @@ func (ig inferenceGroup) HealthCheck(ctx context.Context, w http.ResponseWriter,
 	// TODO: implemnet 503 Text generation inference is down logic
 
 	fmt.Println("HealthCheck served")
+
+	return delphiweb.Respond(ctx, w, struct{}{}, http.StatusOK)
+}
+
+func (ig inferenceGroup) GenerateTokens(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	// TODO: implement tracer support
+
+	
+
+	fmt.Println("Tokens generated")
 
 	return delphiweb.Respond(ctx, w, struct{}{}, http.StatusOK)
 }
