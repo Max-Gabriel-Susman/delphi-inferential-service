4d414aa3451857ce371365e5e27924d716914c1c
Gabe Susman <gabe.susman@gmail.com>
2023-11-25 14:29:51 -0700

    restored inferential capability

--- internal/textgeneration/server.go
+++ internal/textgeneration/server.go
@@ -3,15 +3,19 @@ package textgeneration
 import (
 	"context"
 	"flag"
-	"fmt"
-	"log"
 
 	"github.com/Max-Gabriel-Susman/delphi-inferential-service/internal/clients/openai"
 	pb "github.com/Max-Gabriel-Susman/delphi-inferential-service/textgeneration"
 )
 
+const defaultName = "world"
+
 var (
+	// we need to parameterize and resolve these addr redundancies l8r
+	// addr = flag.String("addr", "10.96.0.3:50052", "the address to connect to")
+	// addr = flag.String("addr", "10.100.0.3:50052", "the address to connect to")
 	addr = flag.String("addr", "localhost:50053", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
 )
 
 type Server interface {
@@ -34,28 +38,4 @@ func NewTextGenerationServer(openaiClient *openai.Client) *TextGenerationServer 
 			OpenAIClient: openaiClient,
 		},
 	}
-}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	r := openai.CreateCompletionsRequest{
-		Model: "gpt-3.5-turbo",
-		Messages: []openai.Message{
-			{
-				Role:    "user",
-				Content: "Say this is a test!",
-			},
-		},
-		Temperature: 0.7,
-	}
-
-	completions, err := s.OpenAIClient.CreateCompletions(r)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(completions)
-
-	return &pb.HelloReply{Message: "Hello world"}, nil
 }
